99032e88690994428c30bc7b49d423fd1ea631df
GHAZLANE MOHAMMED <med.ghazlane@gmail.com>
2020-01-04 01:44:49 +0100

    Guichet unique CRUD manque (verification mot de passe

---
+++ Views/detailsGuichetUnique.php
@@ -0,0 +1,29 @@
+<?php $row = $statement->fetch();?>
+<div class="container-fluid">
+  <br>
+  <h1 class="h3 mb-2 text-gray-800 m-0 font-weight-bold text-primary" style="color: #E56806">Détail de l'utilisateur du guichet unique N° <strong><?php echo $row['ID_GU']  ?></strong></h1>
+  <br>
+  
+   
+	<div class="card shadow mb-4" style="font-size: 20px; width: 100% ;margin:0 auto; ">
+  		<div class="card-header py-3">
+    		<h4 class="m-0 font-weight-bold text-primary" >Informations</h4>
+  		</div>
+  		<div class="row card-body" style="width: 90%; margin: 0 auto; text-align: left;">
+    		<div class="col">Nom </div>
+    		<div class="col"><strong><?php echo $row['NOM_GU']  ?></strong></div>
+    		<div class="w-100"></div><br>
+    		<div class="col">Prénom</div>
+    		<div class="col"><strong><?php echo $row['PRENOM_GU']  ?></strong></div>
+    		<div class="w-100"></div><br>
+    		<div class="col">Date de naissance </div>
+    		<div class="col"><strong><?php echo $row['DATE_NAISSANCE_GU']?></strong></div>
+    		<div class="w-100"></div><br>
+    		<div class="col">Email</div>
+    		<div class="col"><strong><?php echo $row['EMAIL_GU']?></strong></div>
+    		<div class="w-100"></div><br>
+    		<div class="col">Téléphone</div>
+    		<div class="col"><strong><?php echo $row['TELEPHONE_GU']  ?></strong></div>
+    	</div>
+	</div>
+</div>
